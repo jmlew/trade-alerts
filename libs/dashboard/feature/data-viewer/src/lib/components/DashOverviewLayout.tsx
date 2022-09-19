dbed7ee9d2b2988bec59a0a1d0d4e30aafb1e711
Jason Lewis <jm.lewis@gmail.com>
2022-09-19 12:39:55 +1000

    Convert Overviews panel to controlled and initialise to open.

--- libs/dashboard/feature/data-viewer/src/lib/components/DashOverviewLayout.tsx
+++ libs/dashboard/feature/data-viewer/src/lib/components/DashOverviewLayout.tsx
@@ -1,3 +1,5 @@
+import { SyntheticEvent, useState } from 'react';
+
 import { AlertOverviewInfo } from '@kdb-dash/dashboard/domain';
 import { useDashboardDataContext } from '@kdb-dash/dashboard/feature/data-provider';
 import { AlertOverviews } from '@kdb-dash/dashboard/ui/details';
@@ -27,14 +29,21 @@ const styles = {
 
 export function DashOverviewLayout() {
   const { dashData } = useDashboardDataContext();
+  const [isOpen, setOpen] = useState(true);
+
+  function handleToggleAccordian(event: SyntheticEvent) {
+    setOpen(!isOpen);
+  }
+
   if (dashData == null) {
     return null;
   }
+
   const { alerts } = dashData;
   const overviews: AlertOverviewInfo[] = getAlertOverviews(alerts);
   return (
     <Box sx={styles.root}>
-      <Accordion sx={styles.panel}>
+      <Accordion sx={styles.panel} expanded={isOpen} onChange={handleToggleAccordian}>
         <AccordionSummary expandIcon={<ExpandMoreIcon />} id="overview-header">
           <Typography>Alerts Overview</Typography>
         </AccordionSummary>
