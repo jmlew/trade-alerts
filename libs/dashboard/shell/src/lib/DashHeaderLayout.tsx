8af0cd8ed4042567f34ba846787ff38e23099027
Jason Lewis <jm.lewis@gmail.com>
2022-09-20 15:32:06 +1000

    Add simple Drawer implementation for Alert Updater feature placeholder.

--- libs/dashboard/shell/src/lib/DashHeaderLayout.tsx
+++ libs/dashboard/shell/src/lib/DashHeaderLayout.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { KeyboardEvent, MouseEvent, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { FiltersType, getInitialFilterType } from '@kdb-dash/dashboard/domain';
@@ -11,8 +11,9 @@ import {
 import { FiltersTypeBtns } from '@kdb-dash/dashboard/ui/controls';
 import { ApiStateManager } from '@kdb-dash/shared/data-access';
 import { DividerVert } from '@kdb-dash/shared/ui-common';
+import { themeColors } from '@kdb-dash/shared/ui-styles';
 import { ExpandCircleDownOutlined } from '@mui/icons-material';
-import { Box } from '@mui/material';
+import { Box, Drawer, Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
@@ -30,9 +31,19 @@ const styles = {
   spacer: {
     flexGrow: 1,
   },
+  dwawerPanel: {
+    width: 400,
+    height: '100%',
+    textAlign: 'center',
+    p: 4,
+    backgroundColor: themeColors.backgroundLight,
+    color: 'primary.main',
+    borderLeft: `1px solid ${themeColors.primary.dark}`,
+  },
 };
 
 export function DashHeaderLayout() {
+  const [isOpen, setOpen] = useState(false);
   const [searchParams] = useSearchParams();
   const [type, setType] = useState<FiltersType>(getInitialFilterType(searchParams));
   // TODO: Remove context dependancy and add to container which handles showing the side
@@ -40,6 +51,10 @@ export function DashHeaderLayout() {
   const { dashDataState } = useDashboardDataContext();
   const isPending: boolean =
     dashDataState != null && ApiStateManager.isPending(dashDataState);
+
+  function handleToggleDrawer(state: boolean) {
+    return (event: KeyboardEvent | MouseEvent) => setOpen(state);
+  }
   return (
     <AppBar position="static">
       <Toolbar>
@@ -55,6 +70,7 @@ export function DashHeaderLayout() {
         <SectionSpacer />
         <IconButton
           disabled={isPending}
+          onClick={handleToggleDrawer(!isOpen)}
           edge="end"
           color="primary"
           aria-label="update-alert"
@@ -62,6 +78,11 @@ export function DashHeaderLayout() {
         >
           <ExpandCircleDownOutlined sx={{ transform: 'rotate(90deg)' }} />
         </IconButton>
+        <Drawer anchor="right" open={isOpen} onClose={handleToggleDrawer(false)}>
+          <Box sx={styles.dwawerPanel}>
+            <Typography>(Update Alerts Feature)</Typography>
+          </Box>
+        </Drawer>
       </Toolbar>
     </AppBar>
   );
