65fdc1df3d32f53ec84540c11f7867a84937d228
Jason Lewis <jm.lewis@gmail.com>
2022-09-21 17:33:18 +1000

    Add tooltips to some controls.

--- libs/dashboard/ui/controls/src/lib/BtnDownloadData.tsx
+++ libs/dashboard/ui/controls/src/lib/BtnDownloadData.tsx
@@ -1,7 +1,7 @@
 import { MouseEvent } from 'react';
 
 import { Download } from '@mui/icons-material';
-import { Button } from '@mui/material';
+import { Button, Tooltip } from '@mui/material';
 
 const styles = {
   root: { mx: 1, px: 1, color: 'primary.dark' },
@@ -13,8 +13,10 @@ interface BtnDownloadDataProps {
 
 export function BtnDownloadData({ onClick }: BtnDownloadDataProps) {
   return (
-    <Button size="small" startIcon={<Download />} sx={styles.root} onClick={onClick}>
-      Download
-    </Button>
+    <Tooltip title="Download all data (CSV)" arrow>
+      <Button size="small" startIcon={<Download />} sx={styles.root} onClick={onClick}>
+        Download
+      </Button>
+    </Tooltip>
   );
 }
--- libs/dashboard/ui/controls/src/lib/BtnSideExpand.tsx
+++ libs/dashboard/ui/controls/src/lib/BtnSideExpand.tsx
@@ -1,5 +1,5 @@
 import { ExpandCircleDownOutlined } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
+import { IconButton, Tooltip } from '@mui/material';
 
 export interface BtnSideExpandProps {
   isDisabled: boolean;
@@ -9,18 +9,21 @@ export interface BtnSideExpandProps {
 
 export function BtnSideExpand({ isDisabled, isExpanded, onClick }: BtnSideExpandProps) {
   const transform = isExpanded ? 'rotate(270deg)' : 'rotate(90deg)';
+  const buttonTooltip = (isExpanded ? 'Hide' : 'Show') + ' Alerts Panel';
   return (
-    <IconButton
-      disabled={isDisabled}
-      onClick={onClick}
-      edge="end"
-      color="primary"
-      aria-label="update-alert"
-      sx={{ ml: 2 }}
-    >
-      <ExpandCircleDownOutlined
-        sx={{ transform, transition: 'transform 150ms ease-out' }}
-      />
-    </IconButton>
+    <Tooltip title={buttonTooltip} arrow>
+      <IconButton
+        disabled={isDisabled}
+        onClick={onClick}
+        edge="end"
+        color="primary"
+        aria-label="update-alert"
+        sx={{ ml: 2 }}
+      >
+        <ExpandCircleDownOutlined
+          sx={{ transform, transition: 'transform 150ms ease-out' }}
+        />
+      </IconButton>
+    </Tooltip>
   );
 }
