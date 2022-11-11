0df9df8398a6798cc0efb560714370a081b9cb39
Jason Lewis <jm.lewis@gmail.com>
2022-11-12 08:54:04 +1100

    Add snapshot tests to rendered users list.

--- libs/users/ui/src/lib/UsersList.spec.tsx
+++ libs/users/ui/src/lib/UsersList.spec.tsx
@@ -1,3 +1,5 @@
+import renderer from 'react-test-renderer';
+
 import { fireEvent, render } from '@testing-library/react';
 import { User } from '@trade-alerts/users/domain';
 
@@ -11,29 +13,22 @@ function setup() {
     // default
   }
   const users: User[] = [
-    {
-      id: 1,
-      firstName: 'Jason',
-      lastName: 'Lewis',
-      email: 'jason@localhost.com',
-    },
-    {
-      id: 2,
-      firstName: 'System',
-      lastName: 'Infra',
-      email: 'system@localhost.com',
-    },
-    {
-      id: 3,
-      firstName: 'Administrator',
-      lastName: 'Main',
-      email: 'admin@localhost.com',
-    },
+    { id: 1, firstName: 'Jason', lastName: 'Lewis', email: 'jason@localhost.com' },
+    { id: 2, firstName: 'System', lastName: 'Infra', email: 'system@localhost.com' },
+    { id: 3, firstName: 'Administrator', lastName: 'Main', email: 'admin@localhost.com' },
   ];
   return { users, onEditUser, onDeleteUser };
 }
 
 describe(UsersList, () => {
+  it('should render the component correctly', () => {
+    const { users, onEditUser, onDeleteUser } = setup();
+    const component = renderer.create(
+      <UsersList users={users} onEditUser={onEditUser} onDeleteUser={onDeleteUser} />
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
   it('should render a list item for each user', () => {
     const { users, onEditUser, onDeleteUser } = setup();
     const { getByTestId } = render(
@@ -55,7 +50,7 @@ describe(UsersList, () => {
     const user = users[index];
     const btn = getAllByLabelText('edit')[index];
     expect(btn).toBeDefined();
-    fireEvent.click(btn!);
+    btn && fireEvent.click(btn);
     expect(currentUserId).toBe(user.id);
   });
   it('should call delete on delete button click', () => {
@@ -71,7 +66,7 @@ describe(UsersList, () => {
     const user = users[index];
     const btn = getAllByLabelText('delete')[index];
     expect(btn).toBeDefined();
-    fireEvent.click(btn!);
+    btn && fireEvent.click(btn);
     expect(currentUserId).toBe(user.id);
   });
 });
